c8f6aff85c5ebc3c866218e0ee190d0c687f4b7e
BuildTools <unconfigured@null.spigotmc.org>
2024-02-02 21:31:00 +0800

    改寫成一個類 傳值只要傳一個就好

--- load_models.py
+++ load_models.py
@@ -2,22 +2,27 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 from ultralytics import YOLO
+import easyocr
+class AI_model():
+    def __init__(self):
+        self.dicemodel=None
+        self.wavemodel=None
+        self.reader=None
 
-
-def load_model():
-    # dicetype_num_model = models.mobilenet_v3_large(
-    #     weights=True, progress=True)
-    dicetype_num_model = models.mobilenet_v3_large()
-    num_classes = 64
-    dicetype_num_model.classifier[-1] = nn.Linear(
-        in_features=dicetype_num_model.classifier[-1].in_features, out_features=num_classes)
-    state_dict = torch.load(r'V3model_epoch_8.pth',
-                            map_location=torch.device('cpu'))
-    dicetype_num_model.load_state_dict(state_dict)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    dicetype_num_model = dicetype_num_model.to(device)
-    dicetype_num_model.eval()
-    model = YOLO(
-        r'C:\Users\eric\Downloads\Compressed\yolov8\runs\detect\train\weights\best.pt')
-
-    return dicetype_num_model, model
+    def load_model(self):
+        # dicemodel = models.mobilenet_v3_large(
+        #     weights=True, progress=True)
+        self.dicemodel = models.mobilenet_v3_large()
+        num_classes = 64
+        self.dicemodel.classifier[-1] = nn.Linear(
+            in_features=self.dicemodel.classifier[-1].in_features, out_features=num_classes)
+        state_dict = torch.load(r'V3model_epoch_8.pth',
+                                map_location=torch.device('cpu'))
+        self.dicemodel.load_state_dict(state_dict)
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.dicemodel = self.dicemodel.to(device)
+        self.dicemodel.eval()
+        self.wavemodel = YOLO(
+            r"wave.pt")
+        self.reader = easyocr.Reader(['ch_tra'], gpu=True)
+        
